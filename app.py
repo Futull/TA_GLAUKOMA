d2d286c26d80299333115b349a34f5f94ea4532a
Nadhifatul <90815360+Futull@users.noreply.github.com>
2025-06-26 01:44:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -388,7 +388,7 @@ def Segmentation():
         st.subheader("OD/OC Segmentation")
     
         preprocessed_img = st.session_state['preprocessed_image']
-    
+     
         col1, col2 = st.columns(2)
         with col1:
             st.image(preprocessed_img, caption="Preprocessed Image")
