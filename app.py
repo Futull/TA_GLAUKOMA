793ca6483e1931d349f648faaea05a5b250afa38
Nadhifatul <90815360+Futull@users.noreply.github.com>
2025-06-25 19:34:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -491,13 +491,13 @@ def main():
     # Route to appropriate page
     if page == "Cover":
         Cover()
-    elif page == "Preprocessing":
+    elif page == "1.Preprocessing":
         Preprocessing()
-    elif page == "Segmentation":
+    elif page == "2.Segmentation":
         Segmentation()
-    elif page == "Feature Extraction":
+    elif page == "3.Feature Extraction":
         FeatureExtraction()
-    elif page == "Classification":
+    elif page == "4.Classification":
         Classification()
 
 if __name__ == "__main__":
